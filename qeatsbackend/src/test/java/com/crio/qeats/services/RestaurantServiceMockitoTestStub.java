1e7beaa0f03e2bef56945e06e6f8bfbf635ae364
CrioUser <crio-user@criodo.com>
2023-07-28 08:17:43 +0530

    ME_QEATS_V2_MODULE_MOCKITO

--- qeatsbackend/src/test/java/com/crio/qeats/services/RestaurantServiceMockitoTestStub.java
+++ qeatsbackend/src/test/java/com/crio/qeats/services/RestaurantServiceMockitoTestStub.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.crio.qeats.QEatsApplication;
 import com.crio.qeats.dto.Restaurant;
 import com.crio.qeats.exchanges.GetRestaurantsRequest;
 import com.crio.qeats.exchanges.GetRestaurantsResponse;
@@ -25,14 +26,39 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = {QEatsApplication.class})
+@MockitoSettings(strictness = Strictness.STRICT_STUBS)
+@DirtiesContext
+@ActiveProfiles("test")
 public class RestaurantServiceMockitoTestStub {
 
   protected static final String FIXTURES = "fixtures/exchanges";
 
-  protected ObjectMapper objectMapper = new ObjectMapper();
+  //protected ObjectMapper objectMapper = new ObjectMapper();
+  
+  @InjectMocks
+  private RestaurantServiceImpl restaurantService;
+  @MockBean
+  private RestaurantRepositoryService restaurantRepositoryServiceMock;
+  private ObjectMapper objectMapper;
+  
+
+  @BeforeEach
+  void setup() {
+    MockitoAnnotations.initMocks(this);
+
+    objectMapper = new ObjectMapper();
+  }
 
   protected Restaurant restaurant1;
   protected Restaurant restaurant2;
@@ -40,14 +66,24 @@ public class RestaurantServiceMockitoTestStub {
   protected Restaurant restaurant4;
   protected Restaurant restaurant5;
 
-  protected RestaurantServiceImpl restaurantService;
+  // protected RestaurantServiceImpl restaurantService;
 
-  protected RestaurantRepositoryService restaurantRepositoryServiceMock;
+  // protected RestaurantRepositoryService restaurantRepositoryServiceMock;
+  // @BeforeEach
+  // protected RestaurantServiceImpl restaurantService;
+
+  // protected RestaurantRepositoryService restaurantRepositoryServiceMock;
 
   public void initializeRestaurantObjects() throws IOException {
     String fixture =
         FixtureHelpers.fixture(FIXTURES + "/mocking_list_of_restaurants.json");
     Restaurant[] restaurants = objectMapper.readValue(fixture, Restaurant[].class);
+    this.restaurant1 = restaurants[0];
+    this.restaurant2 = restaurants[1];
+    this.restaurant3 = restaurants[2];
+    this.restaurant4 = restaurants[3];
+    this.restaurant5 = restaurants[4];
+    //System.out.println(this.restaurant2.getId());
     // TODO CRIO_TASK_MODULE_MOCKITO
     //  What to do with this Restaurant[] ? Looks unused?
     //  Look for the "assert" statements in the tests
@@ -62,16 +98,20 @@ public class RestaurantServiceMockitoTestStub {
     // Following test case is failing, you have to
     // debug it, find out whats going wrong and fix it.
     // Notes - You can create additional mocks, setup the same and try out.
-
-
-     when(restaurantRepositoryServiceMock
+    initializeRestaurantObjects();
+    when(restaurantRepositoryServiceMock
             .findAllRestaurantsCloseBy(any(Double.class), any(Double.class),
                 eq(LocalTime.of(3, 0)),
-                eq(1.0)))
+                eq(5.0)))
             .thenReturn(Arrays.asList(restaurant1, restaurant2));
+
     GetRestaurantsResponse allRestaurantsCloseBy = restaurantService
         .findAllRestaurantsCloseBy(new GetRestaurantsRequest(20.0, 30.0),
             LocalTime.of(3, 0));
+
+    // System.out.println(allRestaurantsCloseBy.getRestaurants().size());
+    // System.out.println(allRestaurantsCloseBy.getRestaurants());
+    // System.out.println(allRestaurantsCloseBy.getRestaurants());
 
     assertEquals(2, allRestaurantsCloseBy.getRestaurants().size());
     assertEquals("11", allRestaurantsCloseBy.getRestaurants().get(0).getRestaurantId());
@@ -87,9 +127,10 @@ public class RestaurantServiceMockitoTestStub {
 
   @Test
   public void  testFindNearbyWithin3km() throws IOException {
+    initializeRestaurantObjects();
+    List<Restaurant> restaurantList1 = Arrays.asList(restaurant1,restaurant4);
+    List<Restaurant> restaurantList2 = Arrays.asList(restaurant2,restaurant4);
 
-    List<Restaurant> restaurantList1 = null;
-    List<Restaurant> restaurantList2 = null;
 
     // TODO: CRIO_TASK_MODULE_MOCKITO
     //  Initialize these two lists above such that I will match with the assert statements
@@ -106,8 +147,11 @@ public class RestaurantServiceMockitoTestStub {
         .findAllRestaurantsCloseBy(eq(21.0), eq(31.1), eq(LocalTime.of(19, 0)),
             eq(3.0));
 
-    GetRestaurantsResponse allRestaurantsCloseByOffPeakHours;
-    GetRestaurantsResponse allRestaurantsCloseByPeakHours;
+    GetRestaurantsResponse allRestaurantsCloseByOffPeakHours = restaurantService
+        .findAllRestaurantsCloseBy(new GetRestaurantsRequest(20.0, 30.2),
+        LocalTime.of(3, 0));
+
+
 
     // TODO: CRIO_TASK_MODULE_MOCKITO
     //  Call restaurantService.findAllRestaurantsCloseBy with appropriate parameters such that
@@ -115,11 +159,13 @@ public class RestaurantServiceMockitoTestStub {
     //  Our assessment will verify whether these mocks are called as per the definition.
     //  Refer to the assertions below in order to understand the requirements better.
 
-
     assertEquals(2, allRestaurantsCloseByOffPeakHours.getRestaurants().size());
     assertEquals("11", allRestaurantsCloseByOffPeakHours.getRestaurants().get(0).getRestaurantId());
     assertEquals("14", allRestaurantsCloseByOffPeakHours.getRestaurants().get(1).getRestaurantId());
 
+
+    GetRestaurantsResponse allRestaurantsCloseByPeakHours = restaurantService
+        .findAllRestaurantsCloseBy(new GetRestaurantsRequest(21.0, 31.1), LocalTime.of(19, 0));
 
     assertEquals(2, allRestaurantsCloseByPeakHours.getRestaurants().size());
     assertEquals("12", allRestaurantsCloseByPeakHours.getRestaurants().get(0).getRestaurantId());
@@ -127,4 +173,3 @@ public class RestaurantServiceMockitoTestStub {
   }
 
 }
-
